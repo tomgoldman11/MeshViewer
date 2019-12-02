a98d754f555fe84cd7048ff01e7110188843a951
Tom Goldman <tomgoldman11@gmail.com>
2019-12-02 12:36:20 +0200

    adding Point class (h and cpp)

---
+++ Viewer/include/Point.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct Point
+{
+	double X;
+	double Y;
+
+	Point() : X(0), Y(0) {}
+	Point(double x, double y) :X(x), Y(y) {}
+};
---
+++ Viewer/src/Point.cpp
@@ -0,0 +1,2 @@
+#include "Point.h"
+// nothing to add here yet.
