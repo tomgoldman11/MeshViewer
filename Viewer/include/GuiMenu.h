424cfdf3ec533031894c588433ea7af73adae071
Tom Goldman <tomgoldman11@gmail.com>
2019-12-08 15:22:05 +0200

    creating h and cpp of Gui class

---
+++ Viewer/include/GuiMenu.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <imgui/imgui.h>
+#include "Scene.h"
+
+void DrawImguiMenus(ImGuiIO& io, Scene& scene);
---
+++ Viewer/src/GuiMenu.cpp
@@ -0,0 +1,90 @@
+#pragma once
+#include "ImguiMenus.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <nfd.h>
+#include "Scene.h"
+void DrawImguiMenus(ImGuiIO& io, Scene& scene)
+{
+	/**
+	 * MeshViewer menu
+	 *****
+	 */
+	ImGui::Begin("MeshViewer Menu");
+
+	// Menu Bar
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("Open", "CTRL+O"))
+			{
+				nfdchar_t* outPath = NULL;
+				nfdresult_t result = NFD_OpenDialog("obj;", NULL, &outPath);
+				if (result == NFD_OKAY)
+				{
+					scene.AddModel(Utils::LoadMeshModel(outPath));
+					free(outPath);
+				}
+				else if (result == NFD_CANCEL)
+				{
+				}
+				else
+				{
+				}
+
+			}
+			ImGui::EndMenu();
+		}
+
+		// TODO: Add more menubar items (if you want to)
+		ImGui::EndMainMenuBar();
+	}
+
+	// Controls
+	ImGui::ColorEdit3("Clear Color", (float*)&clear_color);
+	// TODO: Add more controls as needed
+
+	ImGui::End();
+
+	/**
+	 * Imgui demo - you can remove it once you are familiar with imgui
+	 */
+
+	 // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+	if (show_demo_window)
+		ImGui::ShowDemoWindow(&show_demo_window);
+
+	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+	{
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Model Control");                          // Create a window called "Model Contorl" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+		ImGui::Checkbox("Another Window", &show_another_window);
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+	}
+
+	// 3. Show another simple window.
+	if (show_another_window)
+	{
+		ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		ImGui::Text("Hello from another window!");
+		if (ImGui::Button("Close Me"))
+			show_another_window = false;
+		ImGui::End();
+	}
+}
